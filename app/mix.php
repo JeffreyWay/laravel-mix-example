f13b909ea068d86436bcfdb1b5032ee68d64a444
Mark <mark@Marks-iMac.local>
2017-01-09 10:58:19 +0100

    Added exception when file not found in manifest

--- app/mix.php
+++ app/mix.php
@@ -2,7 +2,7 @@
 
 // mix('css/app.css')
 // mix('js/app.js')
-function mix($path, $json = false, $shouldHotReload = false)
+function mix($originalPath, $json = false, $shouldHotReload = false)
 {
     if (! $json) static $json;
     if (! $shouldHotReload) static $shouldHotReload;
@@ -23,11 +23,18 @@ function mix($path, $json = false, $shouldHotReload = false)
 
     $path = collect($json['assetsByChunkName'])
         ->flatten()
-        ->first(function ($compiledFile) use ($path, $json) {
+        ->first(function ($compiledFile) use ($originalPath, $json) {
             $compiledFile = trim(str_replace($json['hash'].'.', '', $compiledFile), '/');
 
-            return $compiledFile === trim($path, '/');
+            return $compiledFile === trim($originalPath, '/');
         });
+
+	if(!$path){
+		throw new Exception(
+			'The file "' . $originalPath . '" could not be located in the Laravel Mix Manifest file. ' .
+			'Please run "npm run webpack" and try again.'
+		);
+	}
 
     return $shouldHotReload ? "http://localhost:8080{$path}" : url($path);
 }
